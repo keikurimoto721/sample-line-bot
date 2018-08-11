dfbb57be275ece5d3247c8c432ce9cb07b0ce638
Kei Kurimoto <kurimoto.kei.721@gmail.com>
2018-08-11 23:29:38 +0900

    降水確率下限値の修正、コメント修正

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,11 +1,11 @@
 module Constants
   # 降水確率の下限値
-  MIN_PER = 10
+  MIN_PER = 30
 
   # 天気予報情報提供元のURL
   WEATHER_NEWS_URL = 'https://www.drk7.jp/weather/xml/13.xml'
 
-  # 天気予報のエリアは東京都を指定
+  # 天気予報のエリアは[4]東京都を指定
   target_area = 'area[4]'
 
   # 結果XMLにおける時間ごとの降水確率の階層
